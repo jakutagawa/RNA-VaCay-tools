f7f14261c0d2b6c922e52e8315807cf52beef956
Jon Akutagawa <jakutagawa@cox.net>
2018-02-24 14:26:16 -0800

    first commit with header

---
+++ generate_random_mutations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Name: Jon Akutagawa
+# Date: 18-02-23
+
+"""
+This script pulls random mutations from the PCAWG consensus variant call list to
+generate a list of mutations in bed-like file for BAMSurgeon.
+
+input: MAF file
+output:
+chr pos1    pos2    vaf base
+22  234234  234237  0.25    A
+"""
